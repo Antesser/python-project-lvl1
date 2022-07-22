90e014cf76fd345c1b70e5707e6402fe78f08579
Antesser <ahriman.kun@gmail.com>
2022-07-22 20:24:36 +0300

    changing is_prime function

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,4 +1,5 @@
 from random import randint
+import math
 
 QUESTION: str = 'Answer "yes" if given number is prime. Otherwise answer "no"'
 
@@ -13,9 +14,7 @@ def game_answer() -> tuple[str, str]:
 
 
 def is_prime(number: int) -> int:
-    if number % 2 == 0:
-        return number == 2
-    divider = 3
-    while divider * divider <= number and number % divider != 0:
-        divider += 2
-    return divider * divider > number
+    for i in range(2, int(math.sqrt(number)) + 1):
+        if (number % i) == 0:
+            return False
+    return True
