1fc324a8ca27dfac680b5d47e60514eff64d6591
Antesser <ahriman.kun@gmail.com>
2022-07-22 20:18:17 +0300

    changes in progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -2,20 +2,20 @@ from random import randint
 
 
 QUESTION: str = 'What number is missing in the progression?'
-START_NUMBER: int = randint(1, 30)
 STEP: int = randint(1, 5)
 END_NUMBER: int = randint(40, 50)
 
 
 def game_answer() -> tuple[str, str]:
-    number_in_question = create_progression().copy()
-    random_index = randint(0, len(create_progression()) - 1)
+    start_number: int = randint(1, 30)
+    number_in_question = create_progression(start_number).copy()
+    random_index = randint(0, len(create_progression(start_number)) - 1)
     number_in_question[random_index] = '..'
     question: str = f'{" ".join(map(str, number_in_question))}'
-    answer = create_progression()[random_index]
+    answer = create_progression(start_number)[random_index]
     return question, str(answer)
 
 
-def create_progression() -> list:
-    random_numbers = [i for i in range(START_NUMBER, 100, STEP)]
+def create_progression(start_number) -> list:
+    random_numbers = [i for i in range(start_number, 100, STEP)]
     return random_numbers
